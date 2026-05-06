9c6ca47ee8fef51f5d600eb60fcd33c9cc339023
Chitan Founé Mallé <mchitanfoune@gmail.com>
2026-05-06 16:40:51 +0000

    ajout des nouveaux fichiers

---
+++ src/main/java/com/app/model/Commentaire.java
@@ -0,0 +1,24 @@
+public class Commentaire{
+     private commentaire idcommentaire;
+     private text idtext;
+     
+     
+       //Les getters
+    public int getIdcommentaire() {
+        return idcommentaire;
+}
+
+    public int getIdtext() {
+        return idtext;
+}
+
+
+//Concstructeur
+
+    public Commentaire(commentaire idcommentaire, text idtext,) {
+        this.idCommentaire = idCommentaire;
+        this.idtext = idtext;
+        
+    }
+
+}
