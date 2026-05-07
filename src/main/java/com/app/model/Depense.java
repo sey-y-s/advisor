3ec7c0397dde4627ddec4b6e2ba7d76da782af52
ibrahimf38 <ibrahimfdiarra10961@gmail.com>
2026-05-07 10:41:40 +0000

    mecommit

--- src/main/java/com/app/model/Depense.java
+++ src/main/java/com/app/model/Depense.java
@@ -1,91 +1,86 @@
 package com.app.model;
-import java.util.ArrayList;
-import java.util.List;
 
-public class depense {
-    
+import java.util.*;
+
+public class Depense {
+
     private Integer idDepense;
     private double montant;
     private String description;
     private LocalDate date;
-    
-    public depense(Integer idDepense,double montant,String description,LocalDate date){
+
+    public Depense(Integer idDepense, double montant, String description, LocalDate date) {
         this.idDepense = idDepense;
         this.montant = montant;
         this.description = description;
         this.date = date;
     }
-    
+
     public Integer getIdDepense() {
         return idDepense;
     }
-    
+
     public double getMontant() {
         return montant;
     }
-    
+
     public String getDescription() {
         return description;
     }
-    
+
     public LocalDate getDate() {
         return date;
     }
-    
-    public void setIdDepense(Integer idDepense){
+
+    public void setIdDepense(Integer idDepense) {
         this.idDepense = idDepense;
     }
-    
-    public void setMontant(double montant){
-        if(montant >= 0){
+
+    public void setMontant(double montant) {
+        if (montant >= 0) {
             this.montant = montant;
-        }else{
-            System.out.println("montant negative");
+        } else {
+            System.out.println("montant negatif");
         }
     }
-    
-    public void setDescription(String description){
+
+    public void setDescription(String description) {
         this.description = description;
     }
-    
+
     public void setDate(LocalDate date) {
         this.date = date;
     }
-    
-    public void afficherDepense(){
-        
-    System.out.println("ID : " + idDepense);
-    System.out.println("Montant : " + montant);
-    System.out.println("Description : " + description);
-    System.out.println("Date : " + date);
-    
-  }
-}
 
+    public void afficherDepense() {
 
-public class gestionnaireDepense {
-    
-    private List<depense> listeDeDepense = new ArrayList<>();
-    
-    public void ajouterDepense(Depense d){
-        this.listeDeDepenses.add(d);
-        System.out.println("la depense a ete ajouter");
+        System.out.println("ID : " + idDepense);
+        System.out.println("Montant : " + montant);
+        System.out.println("Description : " + description);
+        System.out.println("Date : " + date);
     }
-    
-    public void supprimierDepense (Integer idRecherche){
-        for(int i=0; i < listeDeDepense.size(); i++){
-            
+
+    private List<Depense> listeDeDepense = new ArrayList<>();
+
+    public void ajouterDepense(Depense d) {
+        this.listeDeDepense.add(d);
+        System.out.println("la depense a ete ajoutee");
+    }
+
+    public void supprimerDepense(Integer idRecherche) {
+
+        for (int i = 0; i < listeDeDepense.size(); i++) {
+
             Depense d = listeDeDepense.get(i);
-            
-            if(d.getIdDepense().equals(idRecherche)){
-                
+
+            if (d.getIdDepense().equals(idRecherche)) {
+
                 listeDeDepense.remove(i);
-                System.out.println("la depense "+ idRecherche +" a bien ete suprimier");
+                System.out.println("la depense " + idRecherche + " a bien ete supprimee");
                 return;
             }
         }
-        
-        System.out.println("depense non trouvé");
+
+        System.out.println("depense non trouvee");
     }
-    
 }
