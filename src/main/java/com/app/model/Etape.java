4efcfb68afd39b0d52c5110225c44d40714122f3
Amidou DIAGANA <diaganaamidou@gmail.com>
2026-05-08 09:48:22 +0000

    Logique derriere la classe étape

--- src/main/java/com/app/model/Etape.java
+++ src/main/java/com/app/model/Etape.java
@@ -1,78 +1,84 @@
-import com.app.enums.enum_etape;
-import com.app.model.projet;
-import com.app.model.activite;
+package com.app.model;
 
+import com.app.enums.Statut;
 
 public class Etape {
     private Integer idEtape;
     private String titre;
     private String description;
-    private Statut statut;  
+    private Statut statut;
     private Projet projet;
 
-     //Le constructeur
-    public Etape(String titre, String description, Statut statut) {
+    public Etape() {
+    }
+
+    public Etape(String titre, String description, Statut statut, Projet projet) {
         this.titre = titre;
         this.description = description;
         this.statut = statut;
+        this.projet = projet;
     }
-   
-    //Les getters
+
+    public Etape(Integer idEtape, String titre, String description, Statut statut, Projet projet) {
+        this.idEtape = idEtape;
+        this.titre = titre;
+        this.description = description;
+        this.statut = statut;
+        this.projet = projet;
+    }
+
+
+    // Getters et setters
     public Integer getIdEtape() {
         return idEtape;
-}   
+    }
+
+    public void setIdEtape(Integer idEtape) {
+        this.idEtape = idEtape;
+    }
+
     public String getTitre() {
         return titre;
-}       
-    public String getDescription() {
-        return description;
-}       
-    public String getStatut() {
-        return statut;
-}
+    }
 
-        
-    //Les setters
-    public void setIdEtape(int idEtape) {
-        this.idEtape = idEtape;
-}
-    
     public void setTitre(String titre) {
         this.titre = titre;
-}
+    }
+
+    public String getDescription() {
+        return description;
+    }
 
     public void setDescription(String description) {
         this.description = description;
-}
+    }
 
-    public void setStatut(String statut) {
+    public Statut getStatut() {
+        return statut;
+    }
+
+    public void setStatut(Statut statut) {
         this.statut = statut;
-}
-
-    
-    //Les methodes 
-    public void AjouterEtape() {
-        System.out.println("Etape ajoutée avec succes");
-    }
-    
-    public void afficherEtape() {
-        System.out.println("");
-    }
-    
-    public void ModifierEtape() {
-        System.out.println("Etape modifiée avec succes");
-    }
-    
-    public void SupprimerEtape() {
-        System.out.println("Etape supprimée avec succes");
-    }
-    
-    public void suivreProgressionGlobale () {
-        System.out.println("");
-    }
-    
-    public void totalDepenseEtape () {
-        System.out.println("");
     }
 
+    public Projet getProjet() {
+        return projet;
+    }
+
+    public void setProjet(Projet projet) {
+        this.projet = projet;
+    }
+
+
+    // Méthode pour valider et créer une étape
+    @Override
+    public String toString() {
+        return "Etape{" +
+                "idEtape=" + idEtape +
+                ", titre='" + titre + '\'' +
+                ", description='" + description + '\'' +
+                ", statut=" + statut +
+                ", projet=" + (projet != null ? projet.getTitre() : "null") +
+                '}';
+    }
 }
