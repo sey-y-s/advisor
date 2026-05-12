9a59e8e6b39b996045ac89c9ec5c6145ec12cb3d
Ismaïla Sangaré <ismailasangare1604@gmail.com>
2026-05-12 17:48:21 +0000

    Implementation de la logique de l'interface utilisateur

---
+++ src/main/java/UI/LocaliteInterface.java
@@ -0,0 +1,35 @@
+package UI;
+
+import models.Localite;
+import services.LocaliteService;
+import java.util.List;
+import java.util.Scanner;
+
+public class LocaliteInterface {
+    static Scanner clavier= new Scanner(System.in);
+    private LocaliteService localiteService;
+
+    public LocaliteInterface(LocaliteService localiteService){
+        this.localiteService= localiteService;
+    }
+
+
+    public Localite choisirLocalite(){
+        List<Localite> localites= localiteService.getAllLocalites();
+        if(localites == null || localites.isEmpty()){
+            System.out.println("Il n'y a aucune localité enregistré !!!");
+        }
+        System.out.println("=========== Liste des localités ===========");
+        for (int i=0; i<localites.size(); i++){
+            System.out.println(i+1 + " = "+ localites.get(i));
+        }
+        int choix;
+        System.out.println("Choisissez!!!!!!!");
+        choix= clavier.nextInt();
+        do {
+            System.out.println("Entrez le bon numéro");
+        } while (choix<1 || choix>localites.size());
+
+        return localites.get(choix - 1);
+    }
+}
--- src/main/java/tables/LocaliteTable.java
+++ src/main/java/tables/LocaliteTable.java
@@ -4,6 +4,7 @@ import models.Localite;
 import org.jetbrains.annotations.NotNull;
 import repositories.LocaliteRepository;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocaliteTable implements LocaliteRepository {
@@ -42,22 +43,23 @@ public class LocaliteTable implements LocaliteRepository {
         @Override
         public List<Localite> getAll() {
             String sql = "SELECT * FROM localite";
+            List<Localite> localites = new ArrayList<>();
             try (Connection connection = ConnexionBdd.getConnection();
                  PreparedStatement ps = connection.prepareStatement(sql)) {
                 try (ResultSet rs = ps.executeQuery()) {
-                    List<Localite> localites = new ArrayList<>();
+
                     while (rs.next()) {
                         Localite localite = new Localite();
                         localite.setId(rs.getInt("id"));
                         localite.setRegionClient(rs.getString("regionClient"));
                         localites.add(localite);
                     }
-                    return localites;
+
                 }
             } catch (SQLException e) {
                 System.out.println("Erreur lors de la recuperation de la liste des localités");
             }
-            return null;
+            return localites;
         }
 
         @Override
