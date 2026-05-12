2a4e0d316ec260b4cc07c395b1cef0725b71b9b3
sey-y-s <seybayacoub@gmail.com>
2026-05-12 09:59:52 +0000

    nouvelle structure

--- src/main/java/com/app/enums/Satisfaction.java
+++
@@ -1,5 +0,0 @@
-package com.app.enums;
-
-public enum Satisfaction {
-    TresInsatisfait, Insatisfait, Neutre, Satisfait, TresSatisfait
-}
--- src/main/java/com/app/repositories/DomaineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repositories;
-import com.app.model.Domaine;
-import java.util.List;
-public Interface DomaineRepository{
-    void public AjouterDomaine(Domaine domaine);
-    void public ModifierDomaine(Domaine domaine);
-    void public SupprimerDomaine(int id);
-    List<Domaine> AfficherDomaine()
-}
