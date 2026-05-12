c5f24fe7b63a21d23c83db3f2b25dc01a83576ea
Dialla Coulibaly <coulibalydialla233@gmail.com>
2026-05-12 14:08:02 +0000

    ajout de la classe domaine

--- src/main/java/db/ConnexionBdd.java
+++ src/main/java/db/ConnexionBdd.java
@@ -1,4 +1,14 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnexionBdd {
+    public static Connection getConnection () throws SQLException {
+        String URL="jdbc:mysql://localhost:3306/advisor";
+         String USER="root";
+         String PASSWORD="diallacoul";
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
 }
