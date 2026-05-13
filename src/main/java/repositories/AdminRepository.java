fe5425350b92ec596f47ee76a973145cf68d2f99
Blackdev <ibrahimfdiarra10961@gmail.com>
2026-05-13 11:09:25 +0000

    ffgh

---
+++ src/main/java/repositories/AdminRepository.java
@@ -0,0 +1,17 @@
+package repositories;
+
+
+import models.Admin;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AdminRepository {
+
+    void add(Admin admin);
+    Optional<Admin> getById(int id);
+    List<Admin> getAll();
+    void update (int id);
+    void delete(int id);
+
+}
