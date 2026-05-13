20b2c50be8cb7f78e042535a2548b8aa8bf80b5b
Mohamed Maiga <medmo7374@gmail.com>
2026-05-13 12:16:11 +0000

    ajout des fichiers

--- src/main/java/models/Admin.java
+++ src/main/java/models/Admin.java
@@ -1,4 +1,8 @@
 package models;
 
+import models.enums.Role;
+
 public class Admin {
+    public Admin(String nom, String prenom, String email, String motDePasse, Integer telephone, Role role) {
+    }
 }
--- src/main/java/models/Client.java
+++ src/main/java/models/Client.java
@@ -1,4 +1,9 @@
 package models;
 
+import models.enums.Niveau;
+import models.enums.Role;
+
 public class Client {
+    public Client(String nom, String prenom, String email, String motDePasse, Niveau niveau, int i, Object o, Integer telephone, Role role) {
+    }
 }
