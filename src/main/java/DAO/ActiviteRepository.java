485c5f497ef2366a37df47a9fbd0c2f861779c4e
iamabdoulaziz <abdoulazizc867@gmail.com>
2026-05-13 17:25:45 +0000

    Implémentation de mon interface service et son implémentation

--- src/main/java/DAO/ActiviteRepository.java
+++ src/main/java/DAO/ActiviteRepository.java
@@ -1,6 +1,8 @@
 package DAO;
 
 import models.*;
+import models.enums.StatutEtape;
+
 import java.util.List;
 
 
@@ -8,6 +10,6 @@ public interface ActiviteRepository {
     void add(Activite activite);
     Activite getById(int id);
     List<Activite> getAll();
-    void update(int id, Activite activite);
+    void update(int id, StatutEtape statut);
     void delete(int id);
 }
