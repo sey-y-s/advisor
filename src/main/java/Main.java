03d0528d5b0519dfeecc6587101ca6fe66a19545
Dialla Coulibaly <coulibalydialla233@gmail.com>
2026-05-15 11:47:57 +0000

    ajout du choix de domaine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,8 +35,8 @@ public class Main {
        // clientInterface.AfficherCLient(10);
         //projetClientUI.afficherProjetClient();
         //projetClientUI.afficherUnProjetClient(1);
-        //DomaineInterface.Afficher();
-        ActiviteInterface.affichage();
+        DomaineInterface.Afficher();
+        //ActiviteInterface.affichage();
 
 
     }
